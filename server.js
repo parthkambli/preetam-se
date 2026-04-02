ac0e8ce35bc8c0a41cac2f96e6b6e16053466c41
Yatvesh1107 <yatvesh1107@gmail.com>
2026-04-02 18:39:02 +0530

    ff

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const fitnessEnquiryRoutes = require('./routes/fitness-enquiry');
 const schoolAdmissionRoutes = require('./routes/school-admission');
 const followupRoutes = require('./routes/followups');
 const studentRoutes = require('./routes/students');
-const staffRoutes = require('./routes/staffRoutes');
+const staffRoutes = require('./routes/StaffRoutes');
 const activityRoutes = require('./routes/activityRoutes');
 
 
 const healthRecordRoutes = require('./routes/healthRecordRoutes');
-const eventRoutes = require('./routes/eventRoutes');
+const eventRoutes = require('./routes/eventRoutes');a
 
 const fitnessMemberRoutes = require('./routes/fitness-member');
 
